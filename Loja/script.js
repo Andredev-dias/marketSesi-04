cbbe0ec5685911e683bb096763cb9be434863d8c
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2025-08-07 11:25:34 -0300

    opa

--- Loja/script.js
+++ Loja/script.js
@@ -1,11 +1,19 @@
 let produtos
 
-window.onload = function(){
-    var storedUser = localStorage.getItem("usuario")
-    var user = JSON.parse(storedUser)
+window.onload = function() {
+    var storedUser = localStorage.getItem("usuario");
+    var user = JSON.parse(storedUser);
 
-    document.getElementById("user").innerText = user.name
-    document.getElementById("perfil").innerText = user.dataEntrada
-    document.getElementById("idPerfil").innerText = user.id
-
+    var dataEntrada = new Date(user.dataEntrada);
+    var dataFormatada = dataEntrada.toLocaleString("pt-BR", {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit"
+    });
+    
+    document.getElementById("perfil").innerText = dataFormatada;
+    document.getElementById("user").innerText = user.name;
+    document.getElementById("idPerfil").innerText = user.id;
 };
